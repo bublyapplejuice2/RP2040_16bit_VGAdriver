41de2cf59f4e7ff99ad30232e21ce0b1df28cfae
Michael Wei <mw756@cornell.edu>
2023-11-13 17:42:35 -0500

    got vga connection to work

---
+++ Ray_Tracer/main.cpp
@@ -0,0 +1,121 @@
+//==============================================================================================
+// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
+
+// #include "rtweekend.h"
+
+// #include "camera.h"
+// #include "color.h"
+// #include "hittable_list.h"
+// #include "material.h"
+// #include "sphere.h"
+
+#include "vga_graphics.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+
+
+int main(void) {
+    // hittable_list world;
+
+    // auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    // world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    // for (int a = -11; a < 11; a++) {
+    //     for (int b = -11; b < 11; b++) {
+    //         auto choose_mat = random_double();
+    //         point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+    //         if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+    //             shared_ptr<material> sphere_material;
+
+    //             if (choose_mat < 0.8) {
+    //                 // diffuse
+    //                 auto albedo = color::random() * color::random();
+    //                 sphere_material = make_shared<lambertian>(albedo);
+    //                 world.add(make_shared<sphere>(center, 0.2, sphere_material));
+    //             } else if (choose_mat < 0.95) {
+    //                 // metal
+    //                 auto albedo = color::random(0.5, 1);
+    //                 auto fuzz = random_double(0, 0.5);
+    //                 sphere_material = make_shared<metal>(albedo, fuzz);
+    //                 world.add(make_shared<sphere>(center, 0.2, sphere_material));
+    //             } else {
+    //                 // glass
+    //                 sphere_material = make_shared<dielectric>(1.5);
+    //                 world.add(make_shared<sphere>(center, 0.2, sphere_material));
+    //             }
+    //         }
+    //     }
+    // }
+
+    // auto material1 = make_shared<dielectric>(1.5);
+    // world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    // auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    // world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    // auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    // world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    // camera cam;
+
+    // cam.aspect_ratio      = 16.0 / 9.0;
+    // cam.image_width       = 1200;
+    // cam.samples_per_pixel = 10;
+    // cam.max_depth         = 20;
+
+    // cam.vfov     = 20;
+    // cam.lookfrom = point3(13,2,3);
+    // cam.lookat   = point3(0,0,0);
+    // cam.vup      = vec3(0,1,0);
+
+    // cam.defocus_angle = 0.6;
+    // cam.focus_dist    = 10.0;
+
+    // cam.render(world);
+
+    set_sys_clock_khz(252000, true);
+    // initialize stio
+    stdio_init_all();
+
+    initVGA() ;
+
+    // Image
+
+    int image_width = 800;
+    int image_height = 600;
+
+    // Render
+
+    // std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    // printf("P3\n" << %d << ' ' <<  << "\n255\n")
+
+    // for (int j = 0; j < image_height; ++j) {
+    //     for (int i = 0; i < image_width; ++i) {
+    //         auto r = double(i) / (image_width-1);
+    //         auto g = double(j) / (image_height-1);
+    //         auto b = 0;
+
+    //         int ir = static_cast<int>(31.999 * r);
+    //         int ig = static_cast<int>(63.999 * g);
+    //         int ib = static_cast<int>(31.999 * b);
+
+    //         int color = (ir << 11) | (ig << 5) | ib;
+
+            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    //     }
+    // }
+
+    return 0;
+}
---
+++ Ray_Tracer/vga_graphics.h
@@ -0,0 +1,29 @@
+/**
+ * Hunter Adams (vha3@cornell.edu)
+ * 
+ *
+ * HARDWARE CONNECTIONS
+ *  - GPIO 16 ---> VGA Hsync
+ *  - GPIO 17 ---> VGA Vsync
+ *  - RP2040 GND ---> VGA GND
+ *
+ * RESOURCES USED
+ *  - PIO state machines 0, 1, and 2 on PIO instance 0
+ *  - DMA channels 0, 1, 2, and 3
+ *  - 153.6 kBytes of RAM (for pixel color data)
+ *
+ * NOTE
+ *  - This is a translation of the display primitives
+ *    for the PIC32 written by Bruce Land and students
+ *
+ */
+
+
+// Give the I/O pins that we're using some names that make sense - usable in main()
+enum vga_pins {HSYNC=16, VSYNC, RED_PIN, GREEN_PIN, BLUE_PIN} ;
+
+// We can only produce 8 (3-bit) colors, so let's give them readable names - usable in main()
+enum colors {BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE} ;
+
+// VGA primitives - usable in main
+void initVGA(void) ;
