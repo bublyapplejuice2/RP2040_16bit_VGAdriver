38b99db49ab1041ef93cb7241f4e376b94829da1
Michael Wei <mw756@cornell.edu>
2023-11-19 18:09:21 -0500

    writing to memory work

--- Ray_Tracer/main.cpp
+++ Ray_Tracer/main.cpp
@@ -24,6 +24,10 @@
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 
+typedef unsigned short ushort;
+volatile ushort color = 0;
+volatile int count = 0;
+
 // #include "pt_cornell_rp2040_v1.h"
 
 
@@ -32,6 +36,7 @@
 // B = 11-15
 
 volatile int color_to_use;
+
 
 void get_mem_addr(int (&gpio_arr)[19], int (&mem_arr)[19]) {
     mem_arr[18] = gpio_arr[8]; // A18 --> GPIO 10
@@ -77,15 +82,11 @@ void get_gpio_arr(int (&gpio_arr)[19], int (&mem_arr)[19]) {
     gpio_arr[3]   = mem_arr[0]; // A0 --> GPIO 4
 }
 
-void set_color(int color) {
+void set_color(ushort color) {
     if (color < 0 || color >= 65536) {
         printf("color out of range");
         return;
     }
-    // for (int i = 0; i < 16; i++) {
-    //     gpio_put(i, color & 1);
-    //     color >>=1;
-    // }
     gpio_put(0, color & 1);
     color >>= 1;
     gpio_put(1, color & 1);
@@ -120,147 +121,56 @@ void set_color(int color) {
     color >>= 1;
 }
 
-// static PT_THREAD (protothread_vga(struct pt *pt)) {
-//     PT_BEGIN(pt);
-//     for (int i = 0; i < 16; i++) {
-//         gpio_init(i);
-//         gpio_set_dir(i, GPIO_OUT);
-//     }
-//     while (true) {
-//         if (gpio_get(16) == 0) {
-//             set_color(0);
-//         } else {
-//             set_color(color);
-//         }
-//     }
-//     PT_END(pt);
-// }
+void incr_color(uint gpio, uint32_t events) {
+    color++;
+    gpio_set_dir(21, GPIO_OUT);
+    gpio_set_dir(22, GPIO_OUT);
+    count++;
+}
 
 int main(void) {
-    // hittable_list world;
-
-    // auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    // world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
-
-    // for (int a = -11; a < 11; a++) {
-    //     for (int b = -11; b < 11; b++) {
-    //         auto choose_mat = random_double();
-    //         point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-
-    //         if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-    //             shared_ptr<material> sphere_material;
-
-    //             if (choose_mat < 0.8) {
-    //                 // diffuse
-    //                 auto albedo = color::random() * color::random();
-    //                 sphere_material = make_shared<lambertian>(albedo);
-    //                 world.add(make_shared<sphere>(center, 0.2, sphere_material));
-    //             } else if (choose_mat < 0.95) {
-    //                 // metal
-    //                 auto albedo = color::random(0.5, 1);
-    //                 auto fuzz = random_double(0, 0.5);
-    //                 sphere_material = make_shared<metal>(albedo, fuzz);
-    //                 world.add(make_shared<sphere>(center, 0.2, sphere_material));
-    //             } else {
-    //                 // glass
-    //                 sphere_material = make_shared<dielectric>(1.5);
-    //                 world.add(make_shared<sphere>(center, 0.2, sphere_material));
-    //             }
-    //         }
-    //     }
-    // }
-
-    // auto material1 = make_shared<dielectric>(1.5);
-    // world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    // auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    // world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    // auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    // world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-    // camera cam;
-
-    // cam.aspect_ratio      = 16.0 / 9.0;
-    // cam.image_width       = 1200;
-    // cam.samples_per_pixel = 10;
-    // cam.max_depth         = 20;
-
-    // cam.vfov     = 20;
-    // cam.lookfrom = point3(13,2,3);
-    // cam.lookat   = point3(0,0,0);
-    // cam.vup      = vec3(0,1,0);
-
-    // cam.defocus_angle = 0.6;
-    // cam.focus_dist    = 10.0;
-
-    // cam.render(world);
-
     set_sys_clock_khz(258000, true);
-    // initialize stio
     stdio_init_all();
-
-    
-
     initVGA();
 
-    // // Image
-
-    // int image_width = 800;
-    // int image_height = 600;
-
-    // // Render
-
-    // // std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    // // printf("P3\n" << %d << ' ' <<  << "\n255\n")
-
-    // for (int j = 0; j < image_height; ++j) {
-    //     for (int i = 0; i < image_width; ++i) {
-    //         auto r = double(i) / (image_width-1);
-    //         auto g = double(j) / (image_height-1);
-    //         auto b = 0;
-
-    //         int ir = static_cast<int>(31.999 * r);
-    //         int ig = static_cast<int>(63.999 * g);
-    //         int ib = static_cast<int>(31.999 * b);
-
-    //         int color = (ir << 11) | (ig << 5) | ib;
-
-
-
-    //         std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-    //     }
-    // }
-
-    int colors[] = {
-    64624, 18931, 6288, 9830, 20824,
-    36141, 21519, 26624, 51410, 33061,
-    2023, 52754, 16253, 36016, 13760,
-    8542, 37204, 15812, 42434, 2734
-    };
-    int j = 0;
-    int wait = 0;
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i<16; i++) {
         gpio_init(i);
         gpio_set_dir(i, GPIO_OUT);
     }
-    while (true) {
-        if (gpio_get(16) == 0) {
-            set_color(0);
-        } 
-        else {
-            set_color(colors[j]);
-            if (j >= 20) {
-                j = 0;
-            }
-            if (wait >= 2000000) {
-                j++;
-                wait = 0;
-            }
-            wait++;
+
+    gpio_init(21);
+    gpio_set_irq_enabled_with_callback(21, GPIO_IRQ_EDGE_RISE, true, &incr_color);
+    gpio_init(22);
+
+
+    
+    while (count < 480000) {
+        if (color < 64000) {
+            set_color(color);
+            gpio_set_dir(21, GPIO_IN);
+            // Send interrupt to other Pico to go write enable
+            gpio_put(21, 1);
+            // Send interrupt to other Pico to increment address
+            gpio_put(22, 1);
+            gpio_set_dir(22, GPIO_IN);
             
+        } else {
+            // If color is 64000, then set back to 0
+            color = 0;
         }
     }
-    // pt_add_thread(protothread_vga);
+
+    for (int i = 0; i<16; i++) {
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_IN);
+    }
+
+    gpio_init(26);
+    gpio_set_dir(26, GPIO_OUT);
+    gpio_put(26, 1);
+
+    while (true) {
+        sleep_ms(5000);
+    }
     return 0;
 }
